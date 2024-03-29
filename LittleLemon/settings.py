d49c657e54fd70720c0d36390499c7ee56152858
SergioSusanu <Sergio.susanu.developer@gmail.com>
2024-03-29 17:10:55 +0200

    token based authentication

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'LittleLemonDRF',
 ]
 
@@ -131,5 +132,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':3,
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
 }
 
--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -1,7 +1,12 @@
+
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Category, MenuItem
 from .serializers import MenuItemSerializer, CategorySerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
 class CategoriesView(generics.ListCreateAPIView):
@@ -14,3 +19,8 @@ class MenuItemsView(generics.ListCreateAPIView):
     ordering_fields = ['price','inventory']
     filterset_fields = ['price', 'inventory']
     search_fields = ['title']
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def secret(request):
+    return Response({"message:some secret message"})
